d61eac6293f66520e6ba126e457bc2ee75d0c831
palance <palanceli@163.com>
2017-11-26 23:19:13 +0800

    commit changes

--- faceRek/sample.py
+++ faceRek/sample.py
@@ -7,27 +7,23 @@ import numpy
 import dlib
 import cv2
 from skimage import io
+import faceswap
 
-class MainApp(object):
+class FaceUtil(object):
     def __init__(self):
         # 训练好的人脸关键点检测器数据
         self.predictorPath = 'extdata/shape_predictor_68_face_landmarks.dat'
         # 训练好的ResNet人脸识别模型
         self.faceRecModelPath = 'extdata/dlib_face_recognition_resnet_model_v1.dat'
-        self.shapePredictor = dlib.shape_predictor(self.predictorPath) # 人脸关键点监测器
-        self.faceRec = dlib.face_recognition_model_v1(self.faceRecModelPath) # 人脸识别模型
+        self.ShapePredictor = dlib.shape_predictor(self.predictorPath) # 人脸关键点监测器
+        self.FaceRec = dlib.face_recognition_model_v1(self.faceRecModelPath) # 人脸识别模型
+        self.Detector = dlib.get_frontal_face_detector()    # 正脸检测器
 
-        # 组织样本数据 和 待识别数据
-        nSamples = 7
-        # 样本数据n-0.jpg
-        self.candImageFiles = ['images/%d-0.jpg'% (i + 1) for i in range(nSamples) ]
-        self.testImageFiles = [] # 待识别数据n-[1, 10].jpg
-        for nCand in range(nSamples):
-            for idx in range(10):
-                testImage = 'images/%d-%d.jpg' % (nCand + 1, (idx + 1))
-                if not os.path.exists(testImage):
-                    break
-                self.testImageFiles.append(testImage)
+class FaceRek(object):
+    def __init__(self):
+        self.faceUtil = FaceUtil()
+        self.shapePredictor = self.faceUtil.ShapePredictor
+        self.faceRec = self.faceUtil.FaceRec
 
     def getFaceDescriptor(self, imgFile):
         ''' 提取人脸特征描述项'''
@@ -47,31 +43,22 @@ class MainApp(object):
         # 转换为numpu array
         return numpy.array(faceDescriptor)
 
-    def MainProc(self):
-        detector = dlib.get_frontal_face_detector()		# 正脸检测器
-        sp = dlib.shape_predictor(self.predictorPath)	# 人脸关键点检测器
-        facerec = dlib.face_recognition_model_v1(self.faceRecModelPath) # 人脸识别模型
+    def getFaceLandmarks(self, imgFile):
+        ''' 提取人脸关键点 '''
+        img = io.imread(imgFile) # 输入单人头像
+        # 正脸检测器
+        detector = dlib.get_frontal_face_detector()
+        # 检测出人脸区域
+        faces = detector(img, 1)
+        if len(faces) != 1:
+            raise Exception('请确保图片是单人的')
+        faceRect = faces[0]
 
-        candDescriptors = [] # 候选人描述项
-        for imgFile in self.candImageFiles: # 提取候选人特征
-            logging.debug("Processing {} ...".format(imgFile))
-            candDescriptors.append(self.getFaceDescriptor(imgFile))
+        # 提取关键点
+        keyPts = self.shapePredictor(img, faceRect).parts()
+        landmarks = numpy.matrix([[p.x, p.y] for p in keyPts])
+        return landmarks
 
-        for imgFile in self.testImageFiles:
-            testDescriptor = self.getFaceDescriptor(imgFile) # 提取待识别人特征
-
-            distList = []   # 待识别人和每位候选的特征差
-            for candDesc in candDescriptors:
-                distance = numpy.linalg.norm(candDesc - testDescriptor)
-                distList.append(distance)
-
-            # 组织成数据结构 {候选人:特征差}
-            candDistDict = dict(zip(self.candImageFiles, distList)) 
-            # 根据特征差排序
-            sortedCandDistDict = sorted(candDistDict.iteritems(), key = lambda d:d[1])
-            # 距离待识别人最近的候选
-            logging.debug('识别出：%s => %s' % (imgFile, sortedCandDistDict[0][0]))
-  
 class DLibUT(unittest.TestCase):
     def setUp(self):
         logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
@@ -93,11 +80,6 @@ class DLibUT(unittest.TestCase):
         
         cv2.destroyAllWindows()
 
-    def test00(self):
-        ''' 跑主流程 '''
-        app = MainApp()
-        app.MainProc()
-
     def test01(self):
         ''' 从一张多人图片中标出人脸区域 '''
         img = cv2.imread('images/girls.jpg')
@@ -112,38 +94,52 @@ class DLibUT(unittest.TestCase):
       
     def test02(self):
         ''' 打印提取到的人脸描述项  '''
-        app = MainApp()
-        faceDescriptor = app.getFaceDescriptor('images/1-0.jpg')
+        faceRek = FaceRek()
+        faceDescriptor = faceRek.getFaceDescriptor('images/1-0.jpg')
         logging.debug(faceDescriptor)
 
     def test03(self):
         ''' 从candImages图片中挑出testImage那个人 '''
-        app = MainApp()
-        candImageFiles = ['images/%d-0.jpg' % (i + 1) for i in range(2)]
-        testImageFile = 'images/1-1.jpg'
-        testDesc = app.getFaceDescriptor(testImageFile) # 待识别人描述项
-        distList = []
-
+        faceRek = FaceRek()
+        candImageFiles = ['images/%d-0.jpg' % (i + 1) for i in range(7)]
+        
         candDescriptors = [] # 候选人描述项
         for imgFile in candImageFiles: # 提取候选人特征
             logging.debug("Processing {} ...".format(imgFile))
-            candDesc = app.getFaceDescriptor(imgFile)
+            candDesc = faceRek.getFaceDescriptor(imgFile)
             candDescriptors.append(candDesc)
-
-            distance = candDesc - testDesc
-            logging.debug('candDesc - testDesc = %s\n norm=%s' % 
-                            (distance, numpy.linalg.norm(distance)))
-
-            distList.append(numpy.linalg.norm(candDesc - testDesc))
         
-        candDistDict = dict(zip(candImageFiles, distList))
-        logging.debug(candDistDict)
-        sortedCandDistDict = sorted(candDistDict.iteritems(), key = lambda d:d[1])
-        logging.debug(sortedCandDistDict)
-        logging.debug('识别出：%s => %s' % (testImageFile, sortedCandDistDict[0][0]))
+        testImageFiles = ['images/%d-1.jpg' % (i + 1) for i in range(7)]
+        for testImageFile in testImageFiles:
+            testDesc = faceRek.getFaceDescriptor(testImageFile) # 待识别人描述项
+            distList = []
+            for candDesc in candDescriptors:
+                distance = candDesc - testDesc
+                # 打印特征值的差和归一值
+                # logging.debug('candDesc - testDesc = %s\n norm=%s' % 
+                #                 (distance, numpy.linalg.norm(distance)))
+
+                distList.append(numpy.linalg.norm(candDesc - testDesc))
+            
+            candDistDict = dict(zip(candImageFiles, distList))
+            # logging.debug(candDistDict) # 打印{cand : distance}
+            sortedCandDistDict = sorted(candDistDict.iteritems(), key = lambda d:d[1])
+            # logging.debug(sortedCandDistDict) # 打印按照distance排序的{cand : distance}
+            logging.debug('识别出：%s => %s' % (testImageFile, sortedCandDistDict[0][0]))
 
     def test04(self):
-        ''' 识别出人脸关键点 '''
+        ''' 识别出单人图片中人脸的关键点 '''
+        faceRec = FaceRek()
+        imgFile = 'images/1-0.jpg'
+        img = cv2.imread(imgFile)
+        landmarks = faceRec.getFaceLandmarks(imgFile)
+        logging.debug(landmarks)
+        pts = numpy.array([landmarks], numpy.int32)
+        cv2.polylines(img, pts.reshape(-1, 1, 2), True, (0, 255, 0), 2)
+        self.waitToClose(img)
+
+    def test05(self):
+        ''' 识别出多人图片中人脸关键点 '''
         img = cv2.imread('images/girls.jpg')
         rgbImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         detector = dlib.get_frontal_face_detector()
@@ -157,6 +153,51 @@ class DLibUT(unittest.TestCase):
             cv2.polylines(img, pts.reshape(-1, 1, 2), True, (0, 255, 0), 2)
 
         self.waitToClose(img)
+
+    def synthesisFaces(self, faceFile1, faceFile2):
+        im1, landmarks1 = faceswap.read_im_and_landmarks(faceFile1)
+        im2, landmarks2 = faceswap.read_im_and_landmarks(faceFile2)
+
+        M = faceswap.transformation_from_points(landmarks1[faceswap.ALIGN_POINTS],
+                                    landmarks2[faceswap.ALIGN_POINTS])
+
+        mask = faceswap.get_face_mask(im2, landmarks2)
+        warped_mask = faceswap.warp_im(mask, M, im1.shape)
+        combined_mask = numpy.max([faceswap.get_face_mask(im1, landmarks1), warped_mask],
+                                axis=0)
+
+        warped_im2 = faceswap.warp_im(im2, M, im1.shape)
+        warped_corrected_im2 = faceswap.correct_colours(im1, warped_im2, landmarks1)
+
+        output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+        return output_im
+
+    def test06(self):
+        imgFile1 = 'images/me.jpg'
+        imgFile2 = 'images/moji8.jpg'
+        resultFile = 'temp.jpg'
+
+        imgResult = self.synthesisFaces(imgFile1, imgFile2)
+        cv2.imwrite(resultFile, imgResult)
+
+        img1 = cv2.imread(imgFile1)
+        img2 = cv2.imread(imgFile2)
+        imgResult = cv2.imread(resultFile)
+
+        rows1, cols1, channels1 = img1.shape
+        rows2, cols2, channels2 = img2.shape
+        rows3, cols3, channels3 = imgResult.shape
+
+        MARGIN = 10
+        rows = max(rows1, rows2)
+        cols = cols1 + cols2 + cols3 + MARGIN * 3
+        imgTotal = numpy.zeros((rows, cols, 3), numpy.uint8)
+        imgTotal[: rows1, :cols1] = img1
+        imgTotal[: rows2, cols1 + MARGIN : cols1 + MARGIN + cols2] = img2
+        imgTotal[: rows3, cols1 + cols2 + MARGIN * 2 : cols1 + cols2 + cols3 + MARGIN * 2] = imgResult
+        self.waitToClose(imgTotal)
+        os.remove(resultFile)
+
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
